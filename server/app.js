3489638d1a6097e33f3d0ce5cbdafbec5b5a1239
“ethan <eman5797@gmail.com>
2022-08-22 18:21:59 -0400

    added landing page to the express server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const app = express(); //set express to a variable to be used
 
 app.use(express.json()); //sets the req bodies to content-type of json
 
+app.use(express.static('../frontend'))
+
 app.get('/', (req, res) => {
     res.json({
         "Setting up": "the server",
@@ -12,6 +14,6 @@ app.get('/', (req, res) => {
 });
 
 
-const port = 5000;
+const port = 3000;
 app.listen(port, () =>
     console.log(`Server is listening on port ${port}`));
