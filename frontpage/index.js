127f058cde2b8c35a9037d7d8aee762fdae3aff9
“ethan <eman5797@gmail.com>
2022-08-31 19:03:17 -0400

    created the functional slide panel. need to polish

--- frontpage/index.js
+++ frontpage/index.js
@@ -1,10 +1,19 @@
 window.addEventListener("DOMContentLoaded", () => {
     chooseRandomPokeman();
+    topPanelToggle();
 });
 
 const chooseRandomPokeman = function () {
     const randomPkmButton = document.getElementById("random");
     randomPkmButton.addEventListener("click", (event) => {
-        
+
+    });
+};
+
+const topPanelToggle = function () {
+    const topToggleButton = document.querySelector(".top-panel-toggle");
+    topToggleButton.addEventListener("click", () => {
+        const wrapper = document.querySelector(".wrapper");
+        wrapper.classList.toggle("top-panel-open");
     });
 };
