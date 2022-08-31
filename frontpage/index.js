4019a68ba3f950f94a817c614dad923bc545ba8f
“ethan <eman5797@gmail.com>
2022-08-30 23:49:50 -0400

    adding button functionality

--- frontpage/index.js
+++ frontpage/index.js
@@ -1,1 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+    chooseRandomPokeman();
+});
 
+const chooseRandomPokeman = function () {
+    const randomPkmButton = document.getElementById("random");
+    randomPkmButton.addEventListener("click", (event) => {
+        
+    });
+};
